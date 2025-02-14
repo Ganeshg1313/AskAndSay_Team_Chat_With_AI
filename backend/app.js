13c83e85b66f254d3514fe782572baaae2287550
Ganeshg1313 <ganeshcoder333@gmail.com>
2025-02-14 13:05:01 +0530

    added a custom cors policy

--- backend/app.js
+++ backend/app.js
@@ -14,7 +14,6 @@ const app = express();
 
 app.use(morgan("dev")); //The 'dev' argument specifies the logging format. In the 'dev' format, logs appear in a concise, colorful format.
 
-app.use(cors());
 
 app.use(express.json()); //The JSON data is converted into a JavaScript object and attached to req.body
 app.use(express.urlencoded({ extended: true }));
@@ -31,6 +30,15 @@ app.get("/", (req, res) => res.status(200).send("Welcome to the API"));
 
 // Global Error Handler
 app.use((err, req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "https://ask-and-say.vercel.app");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+  );
+  if (req.method === "OPTIONS") {
+    res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+    return res.status(200).json({});
+  }
   console.error(err.stack);
   res
     .status(err.status || 500)
