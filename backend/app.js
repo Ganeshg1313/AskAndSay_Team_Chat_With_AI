395f0f8ce21f41cb416d48d798ed01a4274903c5
Ganeshg1313 <ganeshcoder333@gmail.com>
2025-03-01 11:48:30 +0530

    temp

--- backend/app.js
+++ backend/app.js
@@ -13,10 +13,7 @@ import cors from "cors";
 const app = express();
 
 app.use(morgan("dev")); //The 'dev' argument specifies the logging format. In the 'dev' format, logs appear in a concise, colorful format.
-app.use(cors({
-  origin: "https://ask-and-79pwd94xm-ganesh-vaman-ghodkes-projects.vercel.app", // Replace with your actual frontend domain
-  credentials: true, // Allow cookies and authentication headers
-})); // Allow all origins
+app.use(cors('*')); // Allow all origins
 
 
 app.use(express.json()); //The JSON data is converted into a JavaScript object and attached to req.body
