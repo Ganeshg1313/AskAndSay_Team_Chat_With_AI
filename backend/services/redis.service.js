ed96a7ae10dbf4c9bc4be6fab6066bba5ccae9ab
Ganeshg1313 <ganeshcoder333@gmail.com>
2025-03-01 12:40:38 +0530

    Version 2.0

--- backend/services/redis.service.js
+++ backend/services/redis.service.js
@@ -1,9 +1,8 @@
 // Role: Connect to redis database (cache storage)
 
 import { Redis } from "ioredis";
-import "dotenv/config";
 
-const redisClient = new Redis(process.env.REDIS_HOST);
+const redisClient = new Redis('redis://default:fA3CGypYXm8NZCHdevWwHYE4g58qJ1em@redis-17459.crce179.ap-south-1-1.ec2.redns.redis-cloud.com:17459');
 
 redisClient.on("connect", () => {
   console.log("Redis connected");
@@ -13,3 +12,17 @@ redisClient.on("error", (error) => {
 });
 
 export default redisClient;
+
+// import { Redis } from 'ioredis';
+
+// const redisClient = new Redis('redis://:iDibBfTRiaamo7qL5GGiuSkH9N0Qlj3J@memcached-10647.c301.ap-south-1-1.ec2.redns.redis-cloud.com:10647/12943744');
+
+// redisClient.on('connect', () => {
+//   console.log('Redis connected'); 
+// });
+
+// redisClient.on('error', (error) => {
+//   console.error('Redis error:', error);
+// });
+
+// export default redisClient;
