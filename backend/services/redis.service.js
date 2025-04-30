627fb713b739cf20ab6c21e1f4b8278663225f35
Ganeshg1313 <ganeshcoder333@gmail.com>
2025-04-30 12:30:30 +0530

    version 30.04.2025.05

--- backend/services/redis.service.js
+++ backend/services/redis.service.js
@@ -2,44 +2,44 @@
 
 // src/services/redis.service.js
 // services/redis.service.js
-import Redis from 'ioredis';
-import 'dotenv/config';
+// import Redis from 'ioredis';
+// import 'dotenv/config';
 
-const REDIS_URL = 'redis://default:kOpd5QghcVgEp5TP4h3ioe3YilUVzmZO@memcached-15060.c80.us-east-1-2.ec2.redns.redis-cloud.com:15060';  
-// e.g. "redis://default:YOUR_PASSWORD@...:15060"
+// const REDIS_URL = 'redis://default:kOpd5QghcVgEp5TP4h3ioe3YilUVzmZO@memcached-15060.c80.us-east-1-2.ec2.redns.redis-cloud.com:15060';  
+// // e.g. "redis://default:YOUR_PASSWORD@...:15060"
 
-const redisClient = new Redis(REDIS_URL, {
-  // Enable TLS on that port:
-  tls: {
-    // You may disable certificate verification if needed:
-    // rejectUnauthorized: false,
-  }
-});
-
-redisClient.on('connect', () => {
-  console.log('Redis connected');
-});
-redisClient.on('error', (error) => {
-  console.error('Redis error:', error);
-});
-
-export default redisClient;
-
-
-
-
-// import { Redis } from "ioredis";
-
-// const redisClient = new Redis('redis://default:kOpd5QghcVgEp5TP4h3ioe3YilUVzmZO@memcached-15060.c80.us-east-1-2.ec2.redns.redis-cloud.com:15060');
-
-// redisClient.on("connect", () => {
-//   console.log("Redis connected");
+// const redisClient = new Redis(REDIS_URL, {
+//   // Enable TLS on that port:
+//   tls: {
+//     // You may disable certificate verification if needed:
+//     // rejectUnauthorized: false,
+//   }
 // });
-// redisClient.on("error", (error) => {
-//   console.log(error);
+
+// redisClient.on('connect', () => {
+//   console.log('Redis connected');
+// });
+// redisClient.on('error', (error) => {
+//   console.error('Redis error:', error);
 // });
 
 // export default redisClient;
+
+
+
+
+import { Redis } from "ioredis";
+
+const redisClient = new Redis('redis://default:kOpd5QghcVgEp5TP4h3ioe3YilUVzmZO@memcached-15060.c80.us-east-1-2.ec2.redns.redis-cloud.com:15060');
+
+redisClient.on("connect", () => {
+  console.log("Redis connected");
+});
+redisClient.on("error", (error) => {
+  console.log(error);
+});
+
+export default redisClient;
 
 // import { Redis } from 'ioredis';
 
