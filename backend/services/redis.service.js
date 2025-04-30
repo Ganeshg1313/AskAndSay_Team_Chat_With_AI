f668559d4179ece2f3c40c9c74b2d6441a7ddac7
Ganeshg1313 <ganeshcoder333@gmail.com>
2025-04-30 13:15:41 +0530

    version 30.04.2025.13

--- backend/services/redis.service.js
+++ backend/services/redis.service.js
@@ -1,6 +1,5 @@
 // Role: Connect to redis database (cache storage)
 
-
 import { Redis } from "ioredis";
  import "dotenv/config";
  
@@ -13,67 +12,3 @@ import { Redis } from "ioredis";
  });
  
  export default redisClient;
-
-
-// src/services/redis.service.js
-// services/redis.service.js
-// import Redis from 'ioredis';
-// import 'dotenv/config';
-
-// const REDIS_URL = 'redis://default:kOpd5QghcVgEp5TP4h3ioe3YilUVzmZO@memcached-15060.c80.us-east-1-2.ec2.redns.redis-cloud.com:15060';  
-// // e.g. "redis://default:YOUR_PASSWORD@...:15060"
-
-// const redisClient = new Redis(REDIS_URL, {
-//   // Enable TLS on that port:
-//   tls: {
-//     // You may disable certificate verification if needed:
-//     // rejectUnauthorized: false,
-//   }
-// });
-
-// redisClient.on('connect', () => {
-//   console.log('Redis connected');
-// });
-// redisClient.on('error', (error) => {
-//   console.error('Redis error:', error);
-// });
-
-// export default redisClient;
-
-
-
-
-// import { createClient } from 'redis';
-
-// const redisClient = createClient({
-//     url: 'redis://default:kOpd5QghcVgEp5TP4h3ioe3YilUVzmZO@memcached-15060.c80.us-east-1-2.ec2.redns.redis-cloud.com:15060'
-// });
-
-// redisClient.on('error', err => console.log('Redis Client Error', err));
-
-// async function connectRedis() {
-//     try {
-//         await redisClient.connect();
-//         console.log('Redis client connected successfully');
-//     } catch (error) {
-//         console.error('Error connecting to Redis:', error);
-//     }
-// }
-
-// connectRedis();
-
-// export default redisClient;
-
-// import { Redis } from 'ioredis';
-
-// const redisClient = new Redis('redis://:iDibBfTRiaamo7qL5GGiuSkH9N0Qlj3J@memcached-10647.c301.ap-south-1-1.ec2.redns.redis-cloud.com:10647/12943744');
-
-// redisClient.on('connect', () => {
-//   console.log('Redis connected'); 
-// });
-
-// redisClient.on('error', (error) => {
-//   console.error('Redis error:', error);
-// });
-
-// export default redisClient;
