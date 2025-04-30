58f9a758da0e2b183bbdbe715b9c0eb08e4053c5
Ganeshg1313 <ganeshcoder333@gmail.com>
2025-04-30 13:09:44 +0530

    version 30.04.2025.12

--- backend/services/redis.service.js
+++ backend/services/redis.service.js
@@ -1,5 +1,20 @@
 // Role: Connect to redis database (cache storage)
 
+
+import { Redis } from "ioredis";
+ import "dotenv/config";
+ 
+//  const redisClient = new Redis(process.env.REDIS_HOST);
+ const redisClient = new Redis('redis://default:NqvRujwLj6btPltRH9VvwR5xQBrnerou@redis-18123.c274.us-east-1-3.ec2.redns.redis-cloud.com:18123');
+ 
+ redisClient.on("connect", () => {
+   console.log("Redis connected");
+   redisClient.on("error", (error) => {
+ });
+ 
+ export default redisClient;
+
+
 // src/services/redis.service.js
 // services/redis.service.js
 // import Redis from 'ioredis';
@@ -28,26 +43,26 @@
 
 
 
-import { createClient } from 'redis';
+// import { createClient } from 'redis';
 
-const redisClient = createClient({
-    url: 'redis://default:kOpd5QghcVgEp5TP4h3ioe3YilUVzmZO@memcached-15060.c80.us-east-1-2.ec2.redns.redis-cloud.com:15060'
-});
+// const redisClient = createClient({
+//     url: 'redis://default:kOpd5QghcVgEp5TP4h3ioe3YilUVzmZO@memcached-15060.c80.us-east-1-2.ec2.redns.redis-cloud.com:15060'
+// });
 
-redisClient.on('error', err => console.log('Redis Client Error', err));
+// redisClient.on('error', err => console.log('Redis Client Error', err));
 
-async function connectRedis() {
-    try {
-        await redisClient.connect();
-        console.log('Redis client connected successfully');
-    } catch (error) {
-        console.error('Error connecting to Redis:', error);
-    }
-}
+// async function connectRedis() {
+//     try {
+//         await redisClient.connect();
+//         console.log('Redis client connected successfully');
+//     } catch (error) {
+//         console.error('Error connecting to Redis:', error);
+//     }
+// }
 
-connectRedis();
+// connectRedis();
 
-export default redisClient;
+// export default redisClient;
 
 // import { Redis } from 'ioredis';
 
